a51850f6a16f92d77287733fdda12ee4347c4d55
Hackbright Student <user@mini-002.(none)>
2013-10-23 15:03:52 -0700

    Cleaned up code and changed host.

--- test_file.py
+++ test_file.py
@@ -2,31 +2,40 @@ import socket
 import sys
 import select
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(("localhost", 5555))
+def open_connection(host, port):
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.connect((host, port))
+    return my_socket
 
-running = True
-while running:
-    inputready, outputready, exceptready = select.select([my_socket, sys.stdin],[],[])
 
+def format_message(inputready, my_socket):
     for s in inputready:
 
         if s == my_socket:
             msg = s.recv(1024)
             if msg:
                 print msg
+                return True
             else:
                 print "Disconneted from server!"
-                running = False
+                return False
 
         else:
             line = s.readline()
             my_socket.sendall(line)
+            return True
 
 
-# print "reveived:\n%s" % data 
 
+def main():
+    my_socket = open_connection('10.1.10.100', 5555)
+    running = True
+    while running:
+        inputready, outputready, exceptready = select.select([my_socket, sys.stdin],[],[])
 
-    
+        running = format_message(inputready, my_socket)
 
-my_socket.close()
+    my_socket.close()
+
+main()
+
